f52db4aa58985341a475573dc4f31d4ea221c39b
Minh Hồ <holenhatminh0206@gmail.com>
2021-09-19 16:46:05 +0700

    firstcommit

---
+++ Nhom7/src/Nhom7/Nhom7.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Nhom7;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Nhom7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        desktop dk = new desktop();
+        dk.setVisible(true);
+    }
+    
+}
