a9a2866293f293d462637dd8252ce994ac72d114
Thao Lyyy <thaolydn1805@gmail.com>
2021-09-21 23:00:44 +0700

    thaoly them code

--- src/nhom7_themcode.java
+++ src/nhom7_themcode.java
@@ -1,4 +1,30 @@
-
+import java.util.Scanner;
 public class nhom7_themcode {
+			 public static void main(String args[]){
+				 int n;
+				  Scanner s = new Scanner(System.in);
+				  System.out.printf("Nhap so n: \n");
+				  n = s.nextInt();
+				  if(n>0) {
+					  if(SoNguyeTo(n)==1)
+						  System.out.println(n+" la so nguyen to");
+					  else 
+						  System.out.println(n+" khong phai la so nguyen to");
+				  }
+			 }
 
-}
+			 public static int SoNguyeTo (int n){
+				int dem=0;
+				for(int i=1;i<=n;i++) {
+					if(n%i==0)
+						dem++;
+				}
+				if(dem==2) {
+					return 1;
+				}else return 0;
+			 }
+		}
+
+
+
+
